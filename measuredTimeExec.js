7ab45f4d5bbcf4aa5eed06dddf5b44ddee158469
Ilona Parkhomenko <dn121274piv@gmail.com>
2017-12-15 21:21:57 +0200

    measuredTimeExec

---
+++ measuredTimeExec.js
@@ -0,0 +1,20 @@
+var sum = function (a, b) {
+  return a + b;
+};
+
+var measuredExecutionTime = function (targetFunction) {
+  return function () {
+    console.log(this);
+    var label = 'execution time';
+    console.time(label);
+    var result = targetFunction.apply(this, arguments);
+    console.timeEnd(label);
+    return result;
+  }
+};
+
+var math = {
+  measuredSum: measuredExecutionTime(sum)
+};
+
+console.log(math.measuredSum(5, 5));
